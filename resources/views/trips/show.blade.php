88fe906fed12229887a6fc918c55d15e0a63e58e
Sean Mai <seandmai@gmail.com>
2019-04-10 17:27:42 -0600

    Added show-map markers

--- resources/views/trips/show.blade.php
+++ resources/views/trips/show.blade.php
@@ -13,6 +13,12 @@
                     Available Seats: {{ $trip->available_seats }}
                 </div>
                 <div class="">
+                    Pickup location: {{ $trip->pickup }}
+                </div>
+                <div class="">
+                    Dropoff location: {{ $trip->dropoff }}
+                </div>
+                <div class="">
                     <p>Departure Time: {{ $trip->departure_time }}</p>
                 </div>
                 <div class="">
@@ -56,51 +62,38 @@
     <script>
     function initMap() {
         var trip = {!! json_encode($trip->toArray(), JSON_HEX_TAG) !!};
-        console.log(trip);
-
-        var markers = []
+        console.log(trip.title);
         var map = new google.maps.Map(document.getElementById('show-map'), {
           zoom: 11,
           center: {lat: parseFloat(parseLatitude(trip.pickup)), lng: parseFloat(parseLongitude(trip.pickup))}
         });
-        // trips.forEach(function(trip){
-        //     markers.push(new google.maps.Marker({
-        //         position: {lat: parseFloat(parseLatitude(trip.pickup)), lng: parseFloat(parseLongitude(trip.pickup))},
-        //         map: map,
-        //         title: trip.title
-        //    }))
-        // });
+        var pickupMarker = new google.maps.Marker({
+            position: {lat: parseFloat(parseLatitude(trip.pickup)), lng: parseFloat(parseLongitude(trip.pickup))},
+            map: map,
+            title: "Pickup Location",
+            label: "A"
+        });
+        var dropoffMarker = new google.maps.Marker({
+            position: {lat: parseFloat(parseLatitude(trip.dropoff)), lng: parseFloat(parseLongitude(trip.dropoff))},
+            map: map,
+            title: "Dropoff Location",
+            label: "B"
+        });
 
-        // var contentString = '<div id="content">'+
-        //     '<div id="siteNotice">'+
-        //     '</div>'+
-        //     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        //     '<div id="bodyContent">'+
-        //     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        //     'sandstone rock formation in the southern part of the '+
-        //     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        //     'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        //     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        //     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        //     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        //     'Aboriginal people of the area. It has many springs, waterholes, '+
-        //     'rock caves and ancient paintings. Uluru is listed as a World '+
-        //     'Heritage Site.</p>'+
-        //     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        //     'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        //     '(last visited June 22, 2009).</p>'+
-        //     '</div>'+
-        //     '</div>';
-        //
-        // var infowindow = new google.maps.InfoWindow({
-        //   content: contentString
-        // });
-
-
-
-        // marker.addListener('click', function() {
-        //   infowindow.open(map, marker);
-        // });
+        var pickupContent = '<div>Pickup Location:</div> <div>' + trip.pickup + '</div>'
+        var dropoffContent = '<div>Dropoff Location</div> <div>' + trip.dropoff + '</div>'
+        var pickupInfowindow = new google.maps.InfoWindow({
+          content: pickupContent
+        });
+        var dropoffInfowindow = new google.maps.InfoWindow({
+          content: dropoffContent
+        });
+        pickupMarker.addListener('click', function() {
+          pickupInfowindow.open(map, pickupMarker);
+        });
+        dropoffMarker.addListener('click', function() {
+          dropoffInfowindow.open(map, dropoffMarker);
+        });
     }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfFT6ECPnarIUyb5o0gJHcrig9db8tRfQ&callback=initMap"
